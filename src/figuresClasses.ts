b67cea12d6b52f6c8cb81569f1d4a0bc0b2133c5
Oleh Prokopenko <olehprokopenko1@gmail.com>
2026-05-05 23:34:30 +0300

    fix mistakes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -26,17 +26,17 @@ export class Triangle implements Figure {
     if (max >= numbers[0] + numbers[1]) {
       throw new Error('Invalid triangle sides');
     }
-
-    this.getArea = (): number => {
-      const p = (this.a + this.b + this.c) / 2;
-
-      return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(
-        2,
-      );
-    };
   }
 
-  public getArea: () => number;
+  public getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+
+    return (
+      Math.floor(
+        Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)) * 100,
+      ) / 100
+    );
+  }
 }
 
 export class Circle implements Figure {
@@ -49,13 +49,11 @@ export class Circle implements Figure {
     if (radius <= 0) {
       throw new Error('Радіус кола не може бути <= 0');
     }
-
-    this.getArea = (): number => {
-      return Math.floor(Math.PI * this.radius * this.radius * 100) / 100;
-    };
   }
 
-  public getArea: () => number;
+  public getArea(): number {
+    return Math.floor(Math.PI * this.radius * this.radius * 100) / 100;
+  }
 }
 
 export class Rectangle implements Figure {
@@ -69,29 +67,15 @@ export class Rectangle implements Figure {
     if (width <= 0 || height <= 0) {
       throw new Error('Ширина та висота прямокутника не можуть бути <= 0');
     }
-
-    this.getArea = (): number => {
-      return Math.floor(this.width * this.height * 100) / 100;
-    };
   }
 
-  public getArea: () => number;
+  public getArea(): number {
+    return Math.floor(this.width * this.height * 100) / 100;
+  }
 }
 
 export function getInfo<T extends Figure>(figure: T): string {
-  const area = +figure.getArea().toFixed(2);
+  const area = Number(figure.getArea().toFixed(2));
 
-  if (figure instanceof Triangle) {
-    return `A ${figure.color} ${figure.shape} - ${area}`;
-  }
-
-  if (figure instanceof Circle) {
-    return `A ${figure.color} ${figure.shape} - ${area}`;
-  }
-
-  if (figure instanceof Rectangle) {
-    return `A ${figure.color} ${figure.shape} - ${area}`;
-  }
-
-  return typeof figure;
+  return `A ${figure.color} ${figure.shape} - ${area}`;
 }
